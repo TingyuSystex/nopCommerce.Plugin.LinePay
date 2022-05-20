00903b27600522caf6688d09891cc9b3760b857c
TingyuSystex <103096972+TingyuSystex@users.noreply.github.com>
2022-05-20 17:40:22 +0800

    Config

---
+++ LinePaySettings.cs
@@ -0,0 +1,26 @@
+﻿using Nop.Core.Configuration;
+
+namespace Nop.Plugin.Payments.LinePay
+{
+    /// <summary>
+    /// Represents settings of the PayPal Standard payment plugin
+    /// </summary>
+    public class LinePaySettings : ISettings
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether to use sandbox (testing environment)
+        /// </summary>
+        public bool UseSandbox { get; set; }
+
+        /// <summary>
+        /// Gets or sets Channel ID	
+        /// </summary>
+        public string ChannelId { get; set; }
+
+        /// <summary>
+        /// Gets or sets Channel Secret Key	
+        /// </summary>
+        public string ChannelSecretKey { get; set; }
+
+    }
+}
---
+++ Services/ILinePay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Plugin.Payments.LinePay.Models;
+
+namespace Nop.Plugin.Payments.LinePay.Services
+{
+    public interface ILinePay
+    {
+        Task<List<LinePayResponse>> RequestLinePayAsync();
+    }
+}
